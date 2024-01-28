de9d9e867443c0a76cc5058f4f76619aad6d835d
Eduardo Escaleira <duescaleira@gmail.com>
2024-01-28 14:29:09 -0300

    variaveis

---
+++ Dia01/index.js
@@ -0,0 +1,9 @@
+
+    console.log(`JavaScript funcionando!`)
+
+    // VARIÁVEIS
+    let firstName = `Eduardo`
+    console.log(firstName)
+
+    let lastName = `Escaleira`
+    console.log(lastName)
