c9b41bf68d56cb4a708e616d96bf02da9c5d2c68
Eduardo Escaleira <duescaleira@gmail.com>
2024-01-28 15:10:54 -0300

    operadoresAritmeticos

---
+++ Dia02/index.js
@@ -0,0 +1,13 @@
+// OPERADORES ARITMÉTICOS
+
+let num1 = 3
+let num2 = 2
+
+console.log(num1 + num2) // SOMA
+console.log(num1 - num2) // SUBTRAÇÃO
+console.log(num1 / num2) // DIVISÃO
+console.log(num1 * num2) // MULTIPLICAÇÃO
+console.log(num1 % num2) // RESTO DA DIVISÃO
+console.log(num1 ** num2) // EXPONENCIAL
+console.log(num1 + num2++) // INCREMENTO
+console.log(num1 - num2--) // REDUÇÃO
