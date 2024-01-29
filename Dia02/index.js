547b2eecd73f82ff6122fe7cb74caaec69a9a883
Eduardo Escaleira <duescaleira@gmail.com>
2024-01-29 10:17:18 -0300

    switchECase

--- Dia02/index.js
+++ Dia02/index.js
@@ -71,3 +71,21 @@ if (newDriverSpeed > 110) {
 } else {
     console.log('Driving too slow!')
 }
+
+// SWITCH e CASE
+
+let airport = 'AAA'
+
+switch (airport) {
+    case 'MCO': 
+        console.log('Orlando')
+        break
+    case 'JFK':
+        console.log('John. F. Kennedy')
+        break
+    case 'SEA':
+        console.log('Seattle')
+        break
+    default:
+        console.log('Unknown')
+}
