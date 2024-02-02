6314369655ede6392228aeadc3335d17eb9db0e8
Eduardo Escaleira <duescaleira@gmail.com>
2024-02-02 16:40:09 -0300

    dayFour

--- Dia 04/index.js
+++ Dia 04/index.js
@@ -1,8 +1,20 @@
 const num = [7, 8, 9]
+const final = num.pop() // remove o último item da array
 
-num.push(10, 11, 12) // adiciona no final da array
-num.unshift(1, 20) // adiciona no início
-num.splice(3, 0, 4, 5)
+// num.push(10, 11, 12) // adiciona no final da array
+// num.unshift(1, 20) // adiciona no início
+// num.splice(3, 0, 4, 5)
+
+
 
 
 console.log(num)
+
+const movies = [
+    {id: 1, movieName: 'Dejavu'},
+    {id: 2, movieName: 'Back to the future'},
+    {id: 3, movieName: 'The Matrix'}
+
+]
+
+console.log(movies.find(movies => movies.movieName == 'The Matrix')) // ARROW FUNCTION
